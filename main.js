1d5dfd12c109dabde1e85f226bc9f1aeb723fd20
Soukaina Ouchen <souchen@e3r8p3.1337.ma>
2023-07-13 14:57:42 +0100

    output to screen

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = function()
+{
+    document.querySelector("h1").style.color = "pink"
+}
+
+
+
+/*****************Output to screen**************** */
+
+//---window.alert()
+//---document.write()
+//---console.log()
+
+
+window.alert("hello souchen");
+document.write("hello from 1337 compus");
+console.log("hello js course");
