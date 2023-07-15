aaa2d05a6f695fee4120f988b115aaef0aed90de
Soukaina Ouchen <souchen@e3r8p3.1337.ma>
2023-07-15 09:09:07 +0100

    substring, substr, startsWith and EndsWith

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ console.log(`The test2 value is ${test2 || 300}`);
 
 
 
-/*************************************String methods******************************** */
+/*************************************String methods part1******************************** */
 
 //----charAt()
 //--trim() will remove the spaces
@@ -91,6 +91,7 @@ console.log(theName.charAt(1));
 console.log(theName.length);
 console.log(theName.trim());
 
+/*************************************String methods part2******************************** */
 
 //---indexOf(value [Mand], start [Opt] 0)
 //--lastIndexOf(value [Mand], start [opt] Length)
@@ -126,3 +127,37 @@ console.log(a.repeat(3));
 //------split ->  cut a sentence by using any indice
 
 console.log(a.split(" ")); // split by using space
+
+console.log("split with two parameters");
+console.log(a.split("c", 5)); // split by using c , 5 time
+
+/*************************************String methods part3******************************** */
+
+//----substring(start [Mand], End [Opt] not including End)
+//----substr(srat [Mand], characters to extract)
+//----Includes(Value[Mand], start[Opt] Default 0) [ES6]
+//---StartWith(Value [Mand], Start [Opt] Default 0) [ES6]
+//---endWith(Value [Mand], Length [Opt] Default Full Length) [ES6]
+
+let variable = "souchen from Morroco";
+console.log("impliment substring-->");
+console.log(variable.substring(2,6)); // give from ondex 2 to 5
+console.log("impliment substr-->");
+console.log(variable.substr(2,6)); // give from index 2 to 6
+
+console.log("impliment includes with one element in parameter-->");
+console.log(variable.includes("from")); // check if our sentence containe the word 'from'
+console.log("Impliment includes with two element in parameter-->");
+console.log(variable.includes("souchen", 3));// from the index 3 search for word 'souchen' if it exist return true else return false
+
+
+
+console.log(variable.startsWith("s")); //if our sentence start with character 's' return true sinon return false
+console.log(variable.startsWith("s",3)); //if our sentence from the character index 3 start with character 's' return true sinon return false
+
+console.log("impliment function endsWith() with one parameter-->");
+console.log(variable.endsWith("o")); // if our sentence end with character 'o' return true sinon return false
+console.log("impliment function endsWith with two parameter-->");
+console.log(variable.endsWith("e", 6)); // if our sentence from character 0 to character 6, ends with 'e' return true sinon return false
+
+
